dc7fca4dfee596140a13e1c3e74bdb42a28c039e
Christian Haendel <example@example.com>
2023-12-14 22:16:47 +0200

    Week 5

---
+++ AndOperatorWithRanges/Program.cs
@@ -0,0 +1,34 @@
+﻿// 0 (or below) - Freezing
+// from 0 to 5 - Cold
+// from 5 to 10 - Chilly
+// from 10 to 15 - Cool
+// from 15 to 28 - Warm
+// above 28 - Boiling hot
+
+Console.WriteLine("Enter the current temperature:");
+int currentTemp = Int32.Parse(Console.ReadLine());
+
+if (currentTemp <= 0)
+{
+    Console.WriteLine("Freezing");
+}
+else if (currentTemp > 0 && currentTemp < 5)
+{
+    Console.WriteLine("Cold");
+}
+else if (currentTemp >= 5 && currentTemp < 10)
+{
+    Console.WriteLine("Chilly");
+}
+else if (currentTemp >= 10 && currentTemp < 15)
+{
+    Console.WriteLine("Cool");
+}
+else if (currentTemp >= 15 && currentTemp < 28)
+{
+    Console.WriteLine("Warm");
+}
+else
+{
+    Console.WriteLine("Boiling hot");
+}
---
+++ Week5AndOrLogicalOperators/Program.cs
@@ -0,0 +1,40 @@
+﻿// AND (&&) and OR (||) logical operators
+
+//AND
+//true && true = true
+//true && false = false
+//false && true = false
+//false && false = false
+
+string username, password;   //userName: admin; password: admin1234
+
+Console.WriteLine("Enter Username:");
+username = Console.ReadLine();
+
+Console.WriteLine("Enter password:");
+password = Console.ReadLine();
+
+//OR
+//true || true = true
+//true || false = true
+//false || true = true
+//false || false = false
+
+if (username != "admin" || password != "admin1234") //!= NOT
+{
+    Console.WriteLine("Invalid user credentials");
+}
+else
+{
+    Console.WriteLine("Welcome!");
+}
+
+
+//if  (username == "admin" && password == "admin1234")
+//{
+//    Console.WriteLine("Welcome!");
+//}
+//else
+//{
+//    Console.WriteLine("Invalid user credentials");
+//}
